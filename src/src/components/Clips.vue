af65d17a0b53c698b96b943878528184bfb43806
Yimin Fan (FA Talent) <v-yiminfan@microsoft.com>
2020-10-17 17:15:47 +0800

    add map

--- src/src/components/Clips.vue
+++ src/src/components/Clips.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="clips">
     My Clips
-    <div id="arc"></div>
+    <div id="map"></div>
   </div>
 </template>
 
@@ -10,85 +10,38 @@ import * as d3 from 'd3';
 
 export default {
   name: 'Clips',
-  data() {
-    return {
-      gdp: [
-        {country: "USA", value: 20.5 },
-        {country: "China", value: 13.4 },
-        {country: "Germany", value: 4.0 },
-        {country: "Japan", value: 4.9 },
-        {country: "France", value: 2.8 }
-      ]
-    };
-  },
   mounted() {
-    this.generateArc();
+    //this.generateArc();
+    this.generateMap();
   },
   methods: {
-    generateArc() {
-      const w = 500;
-      const h = 500;
+    generateMap(){
+      let width = 550;
+      let height = 420
+let svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", "translate(0,0)")
 
-      const svg = d3
-        .select("#arc")
-        .append("svg")
-        .attr("width", w)
-        .attr("height", h);
+let projection = d3.geoMercator()
+    .center([104, 38])
+    .scale(500)
+    .translate([width / 2, height / 2])
 
-      const sortedGDP = this.gdp.sort((a, b) => (a.value > b.value ? 1 : -1));
-      const color = d3.scaleOrdinal(d3.schemeDark2);
+let path = d3.geoPath()
+    .projection(projection)
 
-      const max_gdp = d3.max(sortedGDP, o => o.value);
-
-      const angleScale = d3
-        .scaleLinear()
-        .domain([0, max_gdp])
-        .range([0, 1.5 * Math.PI]);
-
-      const arc = d3
-        .arc()
-        .innerRadius((d, i) => (i + 1) * 15)
-        .outerRadius((d, i) => (i + 2) * 15)
-        .startAngle(angleScale(0))
-        .endAngle(d => angleScale(d.value));
-
-      const g = svg.append("g");
-
-      g.selectAll("path")
-        .data(sortedGDP)
-        .enter()
-        .append("path")
-        .attr("d", arc)
-        .attr("fill", (d, i) => color(i))
-        .attr("stroke", "#FFF")
-        .attr("stroke-width", "1px")
-        .on("mouseenter", function() {
-          d3.select(this)
-            .transition()
-            .duration(200)
-            .attr("opacity", 0.5);
-        })
-        .on("mouseout", function() {
-          d3.select(this)
-            .transition()
-            .duration(200)
-            .attr("opacity", 1);
-        });
-
-      g.selectAll("text")
-        .data(this.gdp)
-        .enter()
-        .append("text")
-        .text(d => `${d.country} -  ${d.value} Trillion`)
-        .attr("x", -120)
-        .attr("dy", -4)
-        .attr("y", (d, i) => -(i + 1) * 15)
-        .attr("font-size", "12px");
-
-      g.attr("transform", "translate(150, 120)");
-    }
+d3.json("data/china.json").then((geojson) => {
+    svg.append("path")
+        .attr("d", path(geojson))
+        .attr('fill', 'Lavender')
+        .attr("stroke", "DarkGrey")
+        .attr("stroke-width", 1)
+})
   }
-};
+},
+}
 
 </script>
 
