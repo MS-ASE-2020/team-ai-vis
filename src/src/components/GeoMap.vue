c9062355dd9d55b560d0a5085b84336867920ea7
Zhexin Jin (FA Talent) <v-zhexji@microsoft.com>
2020-10-18 18:42:44 +0800

    init components

---
+++ src/src/components/GeoMap.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="geo-map">
+  </div>
+</template>
+
+<script>
+import * as d3 from 'd3';
+
+export default {
+  name: 'GeoMap'
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/src/components/LineChart.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="line-chart">
+  </div>
+</template>
+
+<script>
+import * as d3 from 'd3';
+
+export default {
+  name: 'LineChart'
+}
+</script>
+
+<style scoped>
+</style>
