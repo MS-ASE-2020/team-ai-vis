3c42fbeff3bcb242699b9b04fd1ec03e2bbc4496
Zhexin Jin (FA Talent) <v-zhexji@microsoft.com>
2020-10-18 18:42:44 +0800

    init components

---
+++ src/src/components/BarChart.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bar-chart">
+  </div>
+</template>
+
+<script>
+import * as d3 from 'd3';
+
+export default {
+  name: 'BarChart'
+}
+</script>
+
+<style scoped>
+</style>
