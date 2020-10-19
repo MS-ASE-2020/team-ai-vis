299f2f06bc8ee1c52b1681442e5e9ca0dd859d7d
Yimin Fan (FA Talent) <v-yiminfan@microsoft.com>
2020-10-19 14:47:12 +0800

    add geo-map

--- src/src/components/GeoMap.vue
+++ src/src/components/GeoMap.vue
@@ -4,10 +4,46 @@
 </template>
 
 <script>
+import * as d3 from 'd3';
+//import "../assets/china.json";
 export default {
-  name: 'GeoMap'
-}
-</script>
+  name: 'GeoMap',
+  mounted() {
+    //this.generateArc();
+    this.generateMap();
+  },
+  methods: {
+    generateMap(){
+      let width = 550;
+      let height = 420;
+      let svg = d3.select(".geo-map").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", "translate(0,0)")
 
+      let projection = d3.geoMercator()
+    .center([104, 38])
+    .scale(500)
+    .translate([width / 2, height / 2])
+
+      let path = d3.geoPath().projection(projection)
+      var mapData = require("@/assets/china.json");
+      console.log(mapData);
+    
+      console.log(mapData);
+      svg.append("path")
+          .attr("d", path(mapData))
+          .attr('fill', 'Lavender')
+          .attr("stroke", "DarkGrey")
+          .attr("stroke-width", 1)
+      
+      
+          
+}
+  },
+}
+
+</script>
 <style scoped>
 </style>
