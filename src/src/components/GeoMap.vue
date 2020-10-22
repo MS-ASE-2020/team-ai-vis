f3a9d3feed252aae8bd0cb2ceac604dbd39f0f59
Zhexin Jin (FA Talent) <v-zhexji@microsoft.com>
2020-10-23 00:30:07 +0800

    mediarecorder

--- src/src/components/GeoMap.vue
+++ src/src/components/GeoMap.vue
@@ -1,48 +1,103 @@
 <template>
   <div class="geo-map">
+    <canvas width="960" height="500"></canvas>
+    <el-button type="primary" @click="generateMap()">Export</el-button>
   </div>
 </template>
 
 <script>
 import * as d3 from 'd3';
+import * as topojson from 'topojson';
+// import * as queue from 'd3-queue';
 //import "../assets/china.json";
 export default {
   name: 'GeoMap',
   mounted() {
     //this.generateArc();
-    this.generateMap();
+    console.log('mounted');
+    // this.generateMap();
   },
   methods: {
     generateMap(){
-      let width = 550;
-      let height = 420;
-      let svg = d3.select(".geo-map").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .append("g")
-    .attr("transform", "translate(0,0)")
+      var canvas = document.querySelector("canvas"),
+          context = canvas.getContext("2d"),
+          width = +canvas.width,
+          height = +canvas.height;
 
-      let projection = d3.geoMercator()
-    .center([104, 38])
-    .scale(500)
-    .translate([width / 2, height / 2])
+      var projection = d3.geoOrthographic().scale(195).translate([width / 2, height / 2]).precision(0.1);
 
-      let path = d3.geoPath().projection(projection)
-      var mapData = require("@/assets/china.json");
-      console.log(mapData);
-    
-      console.log(mapData);
-      svg.append("path")
-          .attr("d", path(mapData))
-          .attr('fill', 'Lavender')
-          .attr("stroke", "DarkGrey")
-          .attr("stroke-width", 1)
-      
-      
-          
-}
+      var path = d3.geoPath().projection(projection).context(context);
+
+      var world = require("@/assets/world-110m.json");
+
+      var land = topojson.feature(world, world.objects.land),
+          mesh = topojson.mesh(world, world.objects.countries, function(a, b) {
+            return a !== b;
+          });
+
+      var data = [],
+        stream = canvas.captureStream(),
+        recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
+
+      var timer = d3.timer(function(t) {
+        t = t / 3000;
+        if (t < 1) {
+          draw(t);
+        } else {
+          recorder.stop();
+          timer.stop();
+        }
+      });
+
+      recorder.ondataavailable = function(event) {
+        if (event.data && event.data.size) {
+          data.push(event.data);
+        }
+      };
+
+      recorder.onstop = () => {
+        var url = URL.createObjectURL(new Blob(data, { type: "video/webm" }));
+        d3.select("canvas").remove();
+        d3.select(".geo-map")
+          .append("video")
+          .attr("src", url)
+          .attr("controls", true)
+          .attr("autoplay", true);
+        d3.select(".geo-map")
+          .append("a")
+          .attr("href", url)
+          .attr("download", "video.webm")
+          .text("Click here to download the file");
+      };
+
+      recorder.start();
+
+      function draw(t) {
+        projection.rotate([360 * t]);
+
+        context.lineWidth = 1;
+
+        context.fillStyle = "#fff";
+        context.fillRect(0, 0, width, height);
+
+        context.strokeStyle = "#222";
+        context.beginPath();
+        path({ type: "Sphere" });
+        context.stroke();
+
+        context.fillStyle = "#222";
+        context.beginPath();
+        path(land);
+        context.fill();
+
+        context.strokeStyle = "#fff";
+        context.beginPath();
+        path(mesh);
+        context.stroke();
+      }
+    }
   },
-}
+};    
 
 </script>
 <style scoped>
