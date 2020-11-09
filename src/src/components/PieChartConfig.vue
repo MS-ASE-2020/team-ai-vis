9bd14a69300351d1b49e6599b7d07105a2a64fb5
Yimin Fan (FA Talent) <v-yiminfan@microsoft.com>
2020-11-09 16:50:11 +0800

    modify piechart

--- src/src/components/PieChartConfig.vue
+++ src/src/components/PieChartConfig.vue
@@ -1,12 +1,30 @@
 <template>
   <div class="pie-chart-config">
-      PieChartConfig
+      <el-form label-width="80px" :model="config">
+      <el-form-item label="delay">
+        <el-input v-model.number="config.delay"></el-input>
+      </el-form-item>
+      <el-form-item label="duration">
+        <el-input v-model.number="config.duration"></el-input>
+      </el-form-item>
+      <el-form-item label="range">
+        <el-input v-model.number="config.range"></el-input>
+      </el-form-item>
+      <el-form-item label="opacity">
+        <el-input v-model.number="config.opacity"></el-input>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'PieChartConfig'
+  name: 'PieChartConfig',
+  computed: {
+    config: function() {
+      return this.$store.state.focusedClip.config;
+    }
+  }
 }
 </script>
 
