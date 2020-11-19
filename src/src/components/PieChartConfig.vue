664c3d2591a7d601aa356fed6ba59a17887e0f3d
Fan Yimin <48177486+fym0503@users.noreply.github.com>
2020-11-19 16:46:57 +0800

    Yimin chart (#27)
    * modify piechart
    
    * fix bug

--- src/src/components/PieChartConfig.vue
+++ src/src/components/PieChartConfig.vue
@@ -13,6 +13,12 @@
       <el-form-item label="opacity">
         <el-input v-model.number="config.opacity"></el-input>
       </el-form-item>
+      <el-form-item label="fontsize">
+        <el-input v-model.number="config.fontsize"></el-input>
+      </el-form-item>
+       <el-form-item label="stroke">
+        <el-input v-model.number="config.strokewidth"></el-input>
+      </el-form-item>
     </el-form>
   </div>
 </template>
