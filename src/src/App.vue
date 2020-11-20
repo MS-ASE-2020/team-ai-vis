5e822b1383310bf5b3de4963475be3276db11a28
Zhexin Jin (FA Talent) <v-zhexji@microsoft.com>
2020-11-20 15:48:50 +0800

    overflow

--- src/src/App.vue
+++ src/src/App.vue
@@ -39,6 +39,9 @@ export default {
   margin: 0;
   padding: 0;
 }
+body {
+  overflow: hidden;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
