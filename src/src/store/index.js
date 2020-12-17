166c0a4221e82e3cfe90edeeae6a11141d37eb81
Yimin Fan (FA Talent) <v-yiminfan@microsoft.com>
2020-12-17 16:09:03 +0800

    format

--- src/src/store/index.js
+++ src/src/store/index.js
@@ -1,12 +1,12 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 const store = new Vuex.Store({
   state: {
     focusedClip: null,
-    clips: []
+    clips: [],
   },
   mutations: {
     deleteClip(state, clip) {
@@ -22,11 +22,9 @@ const store = new Vuex.Store({
 
       if (index == 0) {
         state.focusedClip = state.clips[0];
-      }
-      else {
+      } else {
         state.focusedClip = state.clips[index - 1];
       }
-
     },
     left(state, clip) {
       var index = state.clips.indexOf(clip);
@@ -53,14 +51,14 @@ const store = new Vuex.Store({
       var clip = {
         type: type,
         data: {},
-        config: {}
+        config: {},
       };
       switch (type) {
-        case 'BarChartA':
+        case "BarChartA":
           clip.data = {
             values: [100, 80, 78, 98, 93, 82],
-            names: ['A', 'B', 'C', 'D', 'E', 'F'],
-            title: "Default Title"
+            names: ["A", "B", "C", "D", "E", "F"],
+            title: "Default Title",
           };
           clip.config = {
             delay: 200,
@@ -68,24 +66,24 @@ const store = new Vuex.Store({
             barPadding: 8,
             opacity: 7,
             fontsize: 12,
-            startColor: 'blue',
-            endColor: 'green',
+            startColor: "blue",
+            endColor: "green",
             title: "Default Title",
             bar: {
-              beginColor: 'red',
-              endColor: 'steelblue'
+              beginColor: "red",
+              endColor: "steelblue",
             },
             text: {
-              beginColor: 'steelblue',
-              endColor: 'white'
-            }
+              beginColor: "steelblue",
+              endColor: "white",
+            },
           };
           break;
-        case 'BarChartB':
+        case "BarChartB":
           clip.data = {
             values: [200, 44, 54, 98, 250],
-            names: ['A', 'B', 'C', 'D', 'E'],
-            title: "Default Title"
+            names: ["A", "B", "C", "D", "E"],
+            title: "Default Title",
           };
           clip.config = {
             delay: 200,
@@ -93,155 +91,140 @@ const store = new Vuex.Store({
             barPadding: 8,
             opacity: 7,
             fontsize: 12,
-            startColor: 'blue',
-            endColor: 'red',
+            startColor: "blue",
+            endColor: "red",
             title: "Default Title",
             bar: {
-              beginColor: 'red',
-              endColor: 'steelblue'
+              beginColor: "red",
+              endColor: "steelblue",
             },
             text: {
-              beginColor: 'steelblue',
-              endColor: 'white'
-            }
+              beginColor: "steelblue",
+              endColor: "white",
+            },
           };
           break;
-        case 'GeoMapA':
+        case "GeoMapA":
           clip.config = {
             delay: 200,
             duration: 1000,
             size: 1,
             color: "black",
-            title: "Default Title"
+            title: "Default Title",
           };
           clip.data = {
-            "type": "FeatureCollection",
-            "size": [5, 10, 5, 5, 5, 10],
-            "features": [
+            type: "FeatureCollection",
+            size: [5, 10, 5, 5, 5, 10],
+            features: [
               {
-                "type": "Feature",
-                "properties": {},
-                "geometry": {
-                  "type": "Point",
-                  "coordinates": [
-                    105.84228515625,
-                    30.41078179084589
-                  ]
-                }
+                type: "Feature",
+                properties: {},
+                geometry: {
+                  type: "Point",
+                  coordinates: [105.84228515625, 30.41078179084589],
+                },
               },
               {
-                "type": "Feature",
-                "properties": {},
-                "geometry": {
-                  "type": "Point",
-                  "coordinates": [
-                    114.19189453125,
-                    22.329752304376473
-                  ]
-                }
+                type: "Feature",
+                properties: {},
+                geometry: {
+                  type: "Point",
+                  coordinates: [114.19189453125, 22.329752304376473],
+                },
               },
               {
-                "type": "Feature",
-                "properties": {},
-                "geometry": {
-                  "type": "Point",
-                  "coordinates": [
-                    110.3466796875,
-                    25.443274612305746
-                  ]
-                }
+                type: "Feature",
+                properties: {},
+                geometry: {
+                  type: "Point",
+                  coordinates: [110.3466796875, 25.443274612305746],
+                },
               },
               {
-                "type": "Feature",
-                "properties": {},
-                "geometry": {
-                  "type": "Point",
-                  "coordinates": [
-                    112.3681640625,
-                    34.56085936708384
-                  ]
-                }
-              }
+                type: "Feature",
+                properties: {},
+                geometry: {
+                  type: "Point",
+                  coordinates: [112.3681640625, 34.56085936708384],
+                },
+              },
             ],
-            title: "Default Title"
-          }
+            title: "Default Title",
+          };
           break;
-        case 'GeoMapB':
+        case "GeoMapB":
           clip.config = {
             delay: 200,
             duration: 1000,
             size: 1,
             color: "black",
-            title: "Default Title"
+            title: "Default Title",
           };
           clip.data = {
-            "type": "FeatureCollection",
-            "size": [5, 10, 5, 5, 5],
-            "features": [
+            type: "FeatureCollection",
+            size: [5, 10, 5, 5, 5],
+            features: [
               {
-                "type": "Feature",
-                "properties": {},
-                "geometry": {
-                  "type": "Point",
-                  "coordinates": [
-                    -107.75390625,
-                    39.774769485295465
-                  ]
-                }
+                type: "Feature",
+                properties: {},
+                geometry: {
+                  type: "Point",
+                  coordinates: [-107.75390625, 39.774769485295465],
+                },
               },
               {
-                "type": "Feature",
-                "properties": {},
-                "geometry": {
-                  "type": "Point",
-                  "coordinates": [
-                    -101.77734374999999,
-                    35.10193405724606
-                  ]
-                }
+                type: "Feature",
+                properties: {},
+                geometry: {
+                  type: "Point",
+                  coordinates: [-101.77734374999999, 35.10193405724606],
+                },
               },
               {
-                "type": "Feature",
-                "properties": {},
-                "geometry": {
-                  "type": "Point",
-                  "coordinates": [
-                    -98.349609375,
-                    43.89789239125797
-                  ]
-                }
+                type: "Feature",
+                properties: {},
+                geometry: {
+                  type: "Point",
+                  coordinates: [-98.349609375, 43.89789239125797],
+                },
               },
               {
-                "type": "Feature",
-                "properties": {},
-                "geometry": {
-                  "type": "Point",
-                  "coordinates": [
-                    -92.28515625,
-                    32.76880048488168
-                  ]
-                }
+                type: "Feature",
+                properties: {},
+                geometry: {
+                  type: "Point",
+                  coordinates: [-92.28515625, 32.76880048488168],
+                },
               },
               {
-                "type": "Feature",
-                "properties": {},
-                "geometry": {
-                  "type": "Point",
-                  "coordinates": [
-                    -118.47656249999999,
-                    41.376808565702355
-                  ]
-                }
-              }
+                type: "Feature",
+                properties: {},
+                geometry: {
+                  type: "Point",
+                  coordinates: [-118.47656249999999, 41.376808565702355],
+                },
+              },
             ],
-            title: "Default Title"
+            title: "Default Title",
           };
           break;
-        case 'LineChartA':
+        case "LineChartA":
           clip.data = {
-            values: [[1, 224], [2, 528], [3, 756], [4, 632], [5, 582], [6, 704],
-            [7, 766], [8, 804], [9, 884], [10, 960], [11, 1095], [12, 1250]],
-            title: "Default Title"
+            values: [
+              [1, 224],
+              [2, 528],
+              [3, 756],
+              [4, 632],
+              [5, 582],
+              [6, 704],
+              [7, 766],
+              [8, 804],
+              [9, 884],
+              [10, 960],
+              [11, 1095],
+              [12, 1250],
+            ],
+            title: "Default Title",
           };
           clip.config = {
             delay: 200,
@@ -249,36 +232,48 @@ const store = new Vuex.Store({
             strokewidth: 2,
             dotsize: 4,
             title: "Default Title",
-            startColor:'yellow',
-            endColor:"red"
+            startColor: "yellow",
+            endColor: "red",
           };
           break;
-        case 'LineChartB':
+        case "LineChartB":
           clip.data = {
-            values: [[1, 254], [2, 228], [3, 156], [4, 332], [5, 682], [6,404],
-            [7, 366], [8, 204], [9, 184], [10, 560], [11, 395], [12, 250]],
-            title: "Default Title"
+            values: [
+              [1, 254],
+              [2, 228],
+              [3, 156],
+              [4, 332],
+              [5, 682],
+              [6, 404],
+              [7, 366],
+              [8, 204],
+              [9, 184],
+              [10, 560],
+              [11, 395],
+              [12, 250],
+            ],
+            title: "Default Title",
           };
           clip.config = {
             delay: 200,
             duration: 1000,
             strokewidth: 2,
             dotsize: 4,
-            startColor:'green',
-            endColor:"blue",
-            title: "Default Title"
+            startColor: "green",
+            endColor: "blue",
+            title: "Default Title",
           };
           break;
-        case 'PieChartA':
+        case "PieChartA":
           clip.data = {
             values: [
               { country: "A", value: 20 },
               { country: "B", value: 13.4 },
               { country: "C", value: 4.0 },
               { country: "D", value: 4.9 },
-              { country: "E", value: 2.8 }
+              { country: "E", value: 2.8 },
             ],
-            title: "Default Title"
+            title: "Default Title",
           };
           clip.config = {
             delay: 200,
@@ -287,16 +282,14 @@ const store = new Vuex.Store({
             opacity: 8,
             fontsize: 12,
             strokewidth: 1,
-            title: "Default Title"
+            title: "Default Title",
           };
           break;
-        case 'PieChartB':
+        case "PieChartB":
           clip.data = {
-            values: [
-              15, 15, 40, 30
-            ],
+            values: [15, 15, 40, 30],
             names: ["A", "B", "C", "D"],
-            title: "Default Title"
+            title: "Default Title",
           };
           clip.config = {
             delay: 200,
@@ -305,13 +298,13 @@ const store = new Vuex.Store({
             opacity: 8,
             fontsize: 12,
             strokewidth: 1,
-            title: "Default Title"
+            title: "Default Title",
           };
           break;
       }
       state.focusedClip = clip;
       state.clips.push(clip);
-      console.log(clip.data.values)
+      console.log(clip.data.values);
     },
     updateFocusedClip(state, clip) {
       state.focusedClip = clip;
@@ -321,8 +314,8 @@ const store = new Vuex.Store({
     },
     updateData(state, data) {
       state.focusedClip.data = data;
-    }
-  }
-})
+    },
+  },
+});
 
 export default store;
